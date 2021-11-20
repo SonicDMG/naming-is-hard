a259f2cda5bfb64debae1462bcaf56634b5eb941
Cole Goldsmith <cole.goldsmith@datastax.com>
2021-11-20 03:35:24 -0600

    adding a proof of concept for revealing the words slowly

---
+++ src/api/grpc.js
@@ -0,0 +1,14 @@
+const getSentence = async () => {
+  const functionName = "getSentenceGRPC"; // The name of the function to call in the serverless backend
+  // Asynchronously fetch any "shows_by_name" graphQL data from the Astra DB GraphQL API
+  // using the getShowsAstra serverless function to call out to the
+  // Astra/Stargate graphQL endpoint
+  // https://stargate.io/docs/stargate/1.0/developers-guide/graphql.html
+  const response = await fetch("/.netlify/functions/" + functionName, {
+    method: "POST",
+    body: JSON.stringify({ region: "US" }),
+  });
+  return await response.json();
+};
+
+export { getSentence };
