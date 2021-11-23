c727a273695f454866628873de5ab786aba0a357
Cole Goldsmith <cole.goldsmith@datastax.com>
2021-11-23 00:41:16 -0600

    added region select and a retry button. Added threejs for a SICK background

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import "./App.css";
 import Header from "./components/Header";
 import SentenceLoop from "./components/SentenceLoop";
 import Footer from "./components/Footer";
+import { Canvas } from "@react-three/fiber";
+import { Stars, OrbitControls } from "@react-three/drei";
 
 function App() {
   return (
-    <div className="content">
-      <Header title="Near Us"/>
+    <>
+      <div className="content">
+        <Header title="Near Us" />
 
-      <div className="App-content">
-        <SentenceLoop />
+        <div className="App-content">
+          <SentenceLoop />
+        </div>
+
+        <Footer title="Dashboard" />
       </div>
-      
-      <Footer title="Dashboard"/>
-    </div>
+      <Canvas>
+        <OrbitControls autoRotate autoRotateSpeed={0.2} />
+        <Stars />
+      </Canvas>
+    </>
   );
 }
 
--- src/api/grpc.js
+++ src/api/grpc.js
@@ -1,4 +1,4 @@
-const getSentence = async () => {
+const getSentence = async (region) => {
   const functionName = "getSentenceGRPC"; // The name of the function to call in the serverless backend
   // Asynchronously fetch any "shows_by_name" graphQL data from the Astra DB GraphQL API
   // using the getShowsAstra serverless function to call out to the
@@ -6,7 +6,7 @@ const getSentence = async () => {
   // https://stargate.io/docs/stargate/1.0/developers-guide/graphql.html
   const response = await fetch("/.netlify/functions/" + functionName, {
     method: "POST",
-    body: JSON.stringify({ region: "US" }),
+    body: JSON.stringify({ region: region.toUpperCase() }),
   });
   return await response.json();
 };
